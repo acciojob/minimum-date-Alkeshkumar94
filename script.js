334932b7f4a1b929e0df00765c40444056b934e6
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-01-10 11:41:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function minDate(dates) {
   //write you code here
+	var min=dates[0];
+	for(let i=1;i<dates.length;i++){
+		if(dates[i]<min)
+		{
+			min=dates[i];
+		}
+	}
+	return min;
 }
 
 // Do not change the code
+
 
 var dates = [
   "2023/03/01",
